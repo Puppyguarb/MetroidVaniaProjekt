63f1217f4baad9e7520ce7715f31fc323070eaf4
MikeBuz <mjhblamires@gmail.com>
2024-08-26 14:58:23 -0600

    I DID A THING

---
+++ camera_3d.gd
@@ -0,0 +1,12 @@
+extends Camera3D
+
+
+# Called when the node enters the scene tree for the first time.
+func get_mouse_position():
+
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
